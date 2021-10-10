1e35ed5d7787e96a26dee92656f940385b4216f2
Sawan Kumar <88134243+SawanKumar007@users.noreply.github.com>
2021-10-10 11:15:15 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+            This is About page
+            Notebook is a React app in which we can save our notes on the cloud.
+        </div>
+    )
+}
+
+export default About
